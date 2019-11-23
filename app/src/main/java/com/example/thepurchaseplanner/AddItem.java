5eae5b1742f56cc6377dcb2511a5e37aec0b7b19
SakinaVohra <sakina@technoark.in>
2019-11-23 19:13:10 +0000

    Add Item Page Done

--- app/src/main/java/com/example/thepurchaseplanner/AddItem.java
+++ app/src/main/java/com/example/thepurchaseplanner/AddItem.java
@@ -3,12 +3,33 @@ package com.example.thepurchaseplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddItem extends AppCompatActivity {
 
+    private Button mSaveMore, mOnlySave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        mOnlySave = findViewById(R.id.buttonSave);
+        mSaveMore = findViewById(R.id.buttonAddMore);
+
+        mOnlySave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Item Successfully Added!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mSaveMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Item Successfully Added!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
